7e3ab45d0a97e2889acbb1ff5e80a73cf535be43
Nick Jones <nicolasj@mtu.edu>
2023-08-21 18:41:52 -0400

    Fix footer clipping issues Rename uploaded files based on md5

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 import os
+import hashlib
 
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 
 
 class Tag(models.Model):
@@ -12,8 +14,9 @@ class Tag(models.Model):
 
 
 def project_image_path(instance, filename):
+    hash = instance.preview_image_hash
     ext = os.path.splitext(filename)[1]
-    return f"projects/images/{instance.id}{ext}"
+    return f'projects/images/{hash}{ext.lower()}'
 
 
 class Project(models.Model):
@@ -23,8 +26,16 @@ class Project(models.Model):
     gh_link = models.URLField(blank=True)
     yt_link = models.URLField(blank=True)
     preview_image = models.ImageField(upload_to=project_image_path, blank=True)
+    preview_image_hash = models.CharField(max_length=64, blank=True, editable=False)
     tags = models.ManyToManyField('Tag', related_name='projects', blank=True)
     visible = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        md5 = hashlib.md5()
+        for chunk in self.preview_image.chunks():
+            md5.update(chunk)
+        self.preview_image_hash = md5.hexdigest()
+        super(Project, self).save(*args, **kwargs)
